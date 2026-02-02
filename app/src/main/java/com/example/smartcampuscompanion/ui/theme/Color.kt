0b2201ed3d07e877d7c3c2ff6736cf0cfdd05ad0
Caloyyy18 <carlrouello9@gmail.com>
2026-02-02 23:58:21 +0800

    Add Smart Campus color palette

--- app/src/main/java/com/example/smartcampuscompanion/ui/theme/Color.kt
+++ app/src/main/java/com/example/smartcampuscompanion/ui/theme/Color.kt
@@ -6,6 +6,14 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val BluePrimary = Color(0xFF1565C0)
+val BlueSecondary = Color(0xFF42A5F5)
+
+val BackgroundLight = Color(0xFFF5F7FA)
+val CardBackground = Color(0xFFFFFFFF)
+
+val TextPrimary = Color(0xFF1E1E1E)
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
